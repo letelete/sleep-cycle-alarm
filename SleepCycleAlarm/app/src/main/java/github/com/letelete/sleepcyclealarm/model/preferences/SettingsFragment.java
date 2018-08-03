78217f3ba81c536dfe037fcb46ed0a8ae5a8b174
letelete <brunokawka@gmail.com>
2018-08-03 21:12:48 +0200

    Create listener for settings fragment, add auto summary fill

--- SleepCycleAlarm/app/src/main/java/github/com/letelete/sleepcyclealarm/model/preferences/SettingsFragment.java
+++ SleepCycleAlarm/app/src/main/java/github/com/letelete/sleepcyclealarm/model/preferences/SettingsFragment.java
@@ -1,7 +1,12 @@
 package github.com.letelete.sleepcyclealarm.model.preferences;
 
 import android.os.Bundle;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceManager;
 import android.support.v7.preference.PreferenceFragmentCompat;
+
+import java.util.List;
 
 import github.com.letelete.sleepcyclealarm.R;
 
@@ -10,5 +15,30 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.app_preferences);
+        bindPreferenceSummaryToValue(findPreference("ring_duration"));
+    }
+
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String stringValue = newValue.toString();
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+                preference.setSummary(index >= 0
+                        ? listPreference.getEntries()[index]
+                        : null);
+            }
+            return true;
+        }
+    };
+
+    private static void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
     }
 }
