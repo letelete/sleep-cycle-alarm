b2c94ef275cc6d8361977673670d33a4f6be8969
letelete <brunokawka@gmail.com>
2018-09-03 11:28:11 +0200

    Resolve issue #9

--- SleepCycleAlarm/app/src/test/java/com/gmail/brunokawka/poland/sleepcyclealarm/ui/tabs/addalarm/wakeupat/WakeUpAtItemBuilderTests/isPossibleToCreateNextItemTests.java
+++ SleepCycleAlarm/app/src/test/java/com/gmail/brunokawka/poland/sleepcyclealarm/ui/tabs/addalarm/wakeupat/WakeUpAtItemBuilderTests/isPossibleToCreateNextItemTests.java
@@ -10,6 +10,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+// This class use the following naming convention:
+// Should_ExpectedBehavior_When_StateUnderTest
+
 public class isPossibleToCreateNextItemTests {
 
     private DateTime getDateTimeFormatted(String executionHour) {
