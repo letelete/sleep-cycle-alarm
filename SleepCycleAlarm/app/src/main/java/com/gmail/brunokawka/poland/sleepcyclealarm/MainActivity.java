a02607cc116a7619fb638a7150b00027bef3f70a
letelete <brunokawka@gmail.com>
2018-09-06 20:19:50 +0200

    Remove unused imports

--- SleepCycleAlarm/app/src/main/java/com/gmail/brunokawka/poland/sleepcyclealarm/MainActivity.java
+++ SleepCycleAlarm/app/src/main/java/com/gmail/brunokawka/poland/sleepcyclealarm/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
