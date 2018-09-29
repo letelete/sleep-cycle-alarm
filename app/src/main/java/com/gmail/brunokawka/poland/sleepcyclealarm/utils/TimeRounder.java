f9bc2345975fc8aaa1f6bd76a0a2cb6d170220a3
letelete <brunokawka@gmail.com>
2018-09-29 19:12:27 +0200

    Create TimeRounderTests

--- app/src/main/java/com/gmail/brunokawka/poland/sleepcyclealarm/utils/TimeRounder.java
+++ app/src/main/java/com/gmail/brunokawka/poland/sleepcyclealarm/utils/TimeRounder.java
@@ -3,28 +3,44 @@ package com.gmail.brunokawka.poland.sleepcyclealarm.utils;
 import org.joda.time.DateTime;
 
 public class TimeRounder {
+
     public static DateTime getNearest(DateTime dt) {
         if (isRoundToFullHourNeeded(dt)) {
-            return dt.hourOfDay().roundHalfFloorCopy();
+            return getFullHourRound(dt);
         } else if (isRoundToHalfOfTenNeeded(dt)) {
-            return dt.plusMinutes(getDifferenceBetweenMinuteAndHalfOfTen(dt.getMinuteOfHour()));
+            return getHalfOfTenRound(dt);
         } else {
-            int windowMinutes = 5;
-            return dt.withMinuteOfHour((dt.getMinuteOfHour() / windowMinutes) * windowMinutes)
-                    .minuteOfDay().roundFloorCopy();
+            return getTheClosestTenRound(dt);
         }
     }
 
     private static boolean isRoundToFullHourNeeded(DateTime dt) {
-        return dt.getMinuteOfHour() > 57 || dt.getMinuteOfHour() < 5;
+        return dt.getMinuteOfHour() > 57 || dt.getMinuteOfHour() < 3;
     }
 
     private static boolean isRoundToHalfOfTenNeeded(DateTime dt) {
-        return dt.getMinuteOfHour() % 10 >= 3;
+        int minuteOfMinutesOfHour = dt.getMinuteOfHour() % 10;
+        return minuteOfMinutesOfHour >= 3 && minuteOfMinutesOfHour <= 7;
     }
 
     private static int getDifferenceBetweenMinuteAndHalfOfTen(int minute) {
         int halfOfTen = 5;
         return halfOfTen - (minute % 10);
+    }
+
+    private static DateTime getFullHourRound(DateTime dt) {
+        return dt.hourOfDay().roundHalfFloorCopy();
+    }
+
+    private static DateTime getHalfOfTenRound(DateTime dt) {
+        return dt.plusMinutes(getDifferenceBetweenMinuteAndHalfOfTen(dt.getMinuteOfHour()));
+    }
+
+    private static DateTime getTheClosestTenRound(DateTime dt) {
+        int minuteOfMinutesOfHour = dt.getMinuteOfHour() % 10;
+        int diffToTen = 10 - minuteOfMinutesOfHour;
+        return minuteOfMinutesOfHour >= diffToTen
+                ? dt.plusMinutes(diffToTen)
+                : dt.minusMinutes(minuteOfMinutesOfHour);
     }
 }
