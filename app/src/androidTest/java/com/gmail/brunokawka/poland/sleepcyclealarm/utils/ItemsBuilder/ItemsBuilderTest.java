4220bd17a63979866923c8f73797d353c3ffba20
letelete <brunokawka@gmail.com>
2018-09-30 17:28:59 +0200

    Reorganize objects inside of list in ItemsBuilderTest

--- app/src/androidTest/java/com/gmail/brunokawka/poland/sleepcyclealarm/utils/ItemsBuilder/ItemsBuilderTest.java
+++ app/src/androidTest/java/com/gmail/brunokawka/poland/sleepcyclealarm/utils/ItemsBuilder/ItemsBuilderTest.java
@@ -9,11 +9,11 @@ import org.joda.time.format.DateTimeFormat;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class ItemsBuilderTest {
     private ItemsBuilder itemsBuilder;
@@ -29,7 +29,7 @@ public class ItemsBuilderTest {
             "02/01/1111 09:05",
             "02/01/1111 10:35"
     );
-    private ArrayList<Item> itemsForSleepNowBuildingStrategy;
+    private List<Item> itemsForSleepNowBuildingStrategy;
 
     private final DateTime CURRENT_DATE_FOR_WAKEUPAT_BUILDING_STRATEGY = getDateTime("02/01/2011 08:00");
     private final DateTime EXECUTION_DATE_FOR_WAKEUPAT_BUILDING_STRATEGY = getDateTime("02/01/2011 23:00");
@@ -43,7 +43,7 @@ public class ItemsBuilderTest {
             "02/01/2011 19:45",
             "02/01/2011 21:15"
     );
-    private ArrayList<Item> itemsForWakeUpAtBuildingStrategy;
+    private List<Item> itemsForWakeUpAtBuildingStrategy;
 
 
     private static DateTime getDateTime(String string) {
@@ -77,8 +77,9 @@ public class ItemsBuilderTest {
 
     @Test
     public void testIfCanReturnCurrentDates_For_WakeUpAtBuildingStrategy() {
+        DateTime expected;
         for (int index = 0; index < EXPECTED_FOR_WAKEUPAT_BUILDING_STRATEGY.size(); index++) {
-            DateTime expected = getDateTime(EXPECTED_FOR_WAKEUPAT_BUILDING_STRATEGY.get(index));
+            expected = getDateTime(EXPECTED_FOR_WAKEUPAT_BUILDING_STRATEGY.get(index));
             assertEquals(expected, itemsForWakeUpAtBuildingStrategy.get(index).getCurrentDate());
         }
     }
@@ -125,9 +126,11 @@ public class ItemsBuilderTest {
 
     @Test
     public void testIfCanReturnCorrectSummaries_For_SleepNowBuildingStrategy() {
+        DateTime expectedDate;
+        String expected;
         for (int index = 0; index < EXPECTED_FOR_SLEEPNOW_BUILDING_STRATEGY.size(); index++) {
-            DateTime executionDate = getDateTime(EXPECTED_FOR_SLEEPNOW_BUILDING_STRATEGY.get(index));
-            String expected = ItemContentBuilder.getSummary(CURRENT_DATE_FOR_SLEEPNOW_BUILDING_STRATEGY, executionDate.minusMinutes(ItemsBuilderData.getTimeForFallAsleepInMinutes()));
+            expectedDate = getDateTime(EXPECTED_FOR_SLEEPNOW_BUILDING_STRATEGY.get(index));
+            expected = ItemContentBuilder.getSummary(CURRENT_DATE_FOR_SLEEPNOW_BUILDING_STRATEGY, expectedDate.minusMinutes(ItemsBuilderData.getTimeForFallAsleepInMinutes()));
 
             assertEquals(expected, itemsForSleepNowBuildingStrategy.get(index).getSummary());
         }
@@ -137,9 +140,10 @@ public class ItemsBuilderTest {
     public void testIfCanReturnCorrectSummaries_For_WakeUpAtBuildingStrategy() {
         DateTime expectedDate;
         String expected;
+        DateTime executionDateWithoutTimeToFallAsleep;
         for (int index = 0; index < EXPECTED_FOR_WAKEUPAT_BUILDING_STRATEGY.size(); index++) {
             expectedDate = getDateTime(EXPECTED_FOR_WAKEUPAT_BUILDING_STRATEGY.get(index));
-            DateTime executionDateWithoutTimeToFallAsleep = EXECUTION_DATE_FOR_WAKEUPAT_BUILDING_STRATEGY.minusMinutes(ItemsBuilderData.getTimeForFallAsleepInMinutes());
+            executionDateWithoutTimeToFallAsleep = EXECUTION_DATE_FOR_WAKEUPAT_BUILDING_STRATEGY.minusMinutes(ItemsBuilderData.getTimeForFallAsleepInMinutes());
             expected = ItemContentBuilder.getSummary(expectedDate, executionDateWithoutTimeToFallAsleep);
 
             assertEquals(expected, itemsForWakeUpAtBuildingStrategy.get(index).getSummary());
