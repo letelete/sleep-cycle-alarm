51b75130ce53bb654f1b14945af9477c83b7653b
konstzv <konstzv@gmail.com>
2018-10-06 11:29:44 +0300

    Resolve [Issue #83] Improve preferences (Settings) layout (#91)
    * changed styles, dimensions
    
    * added new screens to preferences xml
    
    * changed MenuActivity to handle multiple screens
    
    * changed SettingsFragment to improve settings style
    
    * added toolbar
    
    * changed style
    
    #Need to find better back icon or close to make theme similar
    
    * bigger margin top to toolbar
    
    Corrections for codacy
    
    * unite toolbar methods in one

--- app/src/main/java/com/gmail/brunokawka/poland/sleepcyclealarm/ui/menu/MenuActivity.java
+++ app/src/main/java/com/gmail/brunokawka/poland/sleepcyclealarm/ui/menu/MenuActivity.java
@@ -1,26 +1,26 @@
 package com.gmail.brunokawka.poland.sleepcyclealarm.ui.menu;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceManager;
-import android.view.View;
-import android.widget.TextView;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.gmail.brunokawka.poland.sleepcyclealarm.R;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MenuActivity extends AppCompatActivity
-    implements MenuContract.MenuView {
+    implements MenuContract.MenuView,
+        PreferenceFragmentCompat.OnPreferenceStartScreenCallback {
 
     private MenuPresenter menuPresenter;
-    private PreferenceFragmentCompat preferenceFragment;
 
-    @BindView(R.id.activityTitleTextView)
-    protected TextView activityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +31,7 @@ public class MenuActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         ButterKnife.bind(this);
-
+        settingToolbar();
         menuPresenter.initializeValueByKeysAndPassedIntent(getString(R.string.key_menu_item_id),
                 getString(R.string.key_menu_item_title),
                 getIntent());
@@ -41,9 +41,53 @@ public class MenuActivity extends AppCompatActivity
         menuPresenter.performActionDependingOnMenuItemIdKey(savedInstanceState);
     }
 
-    public void onCloseActivityButtonClick(View view) {
-        menuPresenter.handleCloseActivityButton();
+    private void settingToolbar() {
+        final Toolbar toolbar = findViewById(R.id.activity_menu_toolbar);
+        if(toolbar!=null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
+
+
+    public void handleScreenChange(String key){
+        setActivityTitle(key);
+        //2 conditions to handle the situation when a fragment is not yet created and recreated on theme change. TODO:make it better
+        if (key.equals(getString(R.string.pref_theme_category)) || key.equals(getString(R.string.pref_alarm_category))){
+            setToolbarBackButtonIcon(android.support.v7.appcompat.R.drawable.abc_ic_ab_back_material);
+        }else{
+            setToolbarBackButtonIcon(R.drawable.button_close_activity_drawable);
+        }
+    }
+
+    private void setToolbarBackButtonIcon(int button_close_activity_drawable) {
+        if (getSupportActionBar()!=null)
+        this.getSupportActionBar().setHomeAsUpIndicator(button_close_activity_drawable);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        handleIconAndTitleDependsOnActiveFragment();
+
+    }
+
+    private void handleIconAndTitleDependsOnActiveFragment() {
+        Fragment frag = getSupportFragmentManager().findFragmentById(R.id.activity_menu_container);
+        if(frag instanceof PreferenceFragmentCompat) {
+            handleScreenChange(((PreferenceFragmentCompat)frag).getPreferenceScreen().getKey());
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     public void setAppTheme(int themeId) {
@@ -52,21 +96,28 @@ public class MenuActivity extends AppCompatActivity
 
     @Override
     public void setActivityTitle(String title) {
-        activityTitle.setText(title);
+        if (getSupportActionBar()!=null)
+        getSupportActionBar().setTitle(title);
     }
 
     @Override
     public void findPreferenceFragment() {
-        preferenceFragment = (SettingsFragment) getSupportFragmentManager()
-                .findFragmentByTag(getString(R.string.key_settings_tag));
+
     }
+
 
     @Override
     public void openNewPreferenceFragment() {
-        preferenceFragment = new SettingsFragment();
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.menu_activity_container, preferenceFragment, getString(R.string.key_settings_tag))
-                .commit();
+
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag(SettingsFragment.TAG);
+        if (fragment == null) {
+            fragment = new SettingsFragment();
+        }
+
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.activity_menu_container, fragment, SettingsFragment.TAG);
+        ft.commit();
     }
 
     @Override
@@ -89,5 +140,22 @@ public class MenuActivity extends AppCompatActivity
     @Override
     public void closeActivity() {
         finish();
+    }
+
+
+
+    @Override
+    public boolean onPreferenceStartScreen(PreferenceFragmentCompat preferenceFragmentCompat,
+            PreferenceScreen preferenceScreen) {
+        handleScreenChange(preferenceScreen.getKey());
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        SettingsFragment fragment = new SettingsFragment();
+        Bundle args = new Bundle();
+        args.putString(PreferenceFragmentCompat.ARG_PREFERENCE_ROOT, preferenceScreen.getKey());
+        fragment.setArguments(args);
+        ft.add(R.id.activity_menu_container, fragment, preferenceScreen.getKey());
+        ft.addToBackStack(preferenceScreen.getKey());
+        ft.commit();
+        return true;
     }
 }
