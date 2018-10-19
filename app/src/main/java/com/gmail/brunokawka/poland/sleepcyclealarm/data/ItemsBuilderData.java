4b7d2c94fe15b2dd2c6ceabccc9b5a3375501b92
letelete <brunokawka@gmail.com>
2018-10-19 21:40:27 +0200

    Remove redundant comment

--- app/src/main/java/com/gmail/brunokawka/poland/sleepcyclealarm/data/ItemsBuilderData.java
+++ app/src/main/java/com/gmail/brunokawka/poland/sleepcyclealarm/data/ItemsBuilderData.java
@@ -3,7 +3,7 @@ package com.gmail.brunokawka.poland.sleepcyclealarm.data;
 public class ItemsBuilderData {
     private final static int MAX_AMOUNT_OF_ITEMS_IN_LIST = 8;
     private final static int ONE_SLEEP_CYCLE_DURATION_IN_MINUTES = 90;
-    private final static int TIME_FOR_FALL_ASLEEP_IN_MINUTES = 15; // TODO: It could be an option in sharedPreferences to let user personalize that
+    private final static int TIME_FOR_FALL_ASLEEP_IN_MINUTES = 15;
 
     public static int getMaxAmountOfItemsInList() {
         return MAX_AMOUNT_OF_ITEMS_IN_LIST;
